96c0422f9787383e2696b10792b691d09780a8f3
Alexander Matheson <alexander.matheson@stu.ocsb.ca>
2023-02-07 13:42:20 +0000

    Added Hello World program

---
+++ HelloWorld.swift
@@ -0,0 +1,11 @@
+//  HelloWorld.swift
+//
+//  Created by Alexander Matheson
+//  Created on 2023-Feb-7
+//  Version 1.0
+//  Copyright (c) 2023 Alexander Matheson. All rights reserved.
+//
+//  This program prints two messages.
+
+print("Hello, World, from Alexander Matheson!")
+print("Finally installed Swift.")
